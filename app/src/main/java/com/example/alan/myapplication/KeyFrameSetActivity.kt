dfde95d55429c6e6beb3a9db4a86ee4320100d79
alan <alan.leng@planetart.cn>
2018-11-12 14:46:40 +0800

    Java to Kotlin

---
+++ app/src/main/java/com/example/alan/myapplication/KeyFrameSetActivity.kt
@@ -0,0 +1,13 @@
+package com.example.alan.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class KeyFrameSetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_key_frame_set)
+    }
+}
