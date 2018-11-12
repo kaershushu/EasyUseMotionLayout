67b924c4259e8cdc4d6c3ad430d0ffe5ecc2d395
alan <alan.leng@planetart.cn>
2018-11-12 14:01:19 +0800

    新的Kotlin分支

---
+++ app/src/main/java/com/example/alan/myapplication/Demo1Activity.kt
@@ -0,0 +1,13 @@
+package com.example.alan.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class Demo1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo1)
+    }
+}
