6561712c9b583174bc1c6a1d5f5ad83add17facf
alan <alan.leng@planetart.cn>
2018-11-12 13:48:40 +0800

    添加新属性

---
+++ app/src/main/java/com/example/alan/myapplication/Demo1Activity.java
@@ -0,0 +1,14 @@
+package com.example.alan.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Demo1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo1);
+    }
+}
---
+++ app/src/main/java/com/example/alan/myapplication/KeyFrameSetActivity.java
@@ -0,0 +1,14 @@
+package com.example.alan.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KeyFrameSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_frame_set);
+    }
+}
